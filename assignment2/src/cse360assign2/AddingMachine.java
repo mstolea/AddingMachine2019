59317186414b5d1404250295b849f8189137677f
mstolea <mstolea@DESKTOP-4HS4HGE>
2019-10-07 14:50:45 -0700

    Initial commit of project.

---
+++ assignment2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,59 @@
+package cse360assign2;
+
+/**
+ * This class returns a integer based on arithmetic operations 
+ * performed with said integer.
+ * @author Maxim Tolea, CSE 360, Assignment 2, 1213323044
+ * @version 1.0 
+ * @since 10/7/2019
+ */
+
+public class AddingMachine {
+ 
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	
+	/**
+	 * This returns the current total of the running operations.
+	 * @return the current total of all the operations
+	 */
+	public int getTotal () {
+		return 0;
+	}
+	
+	
+	/**
+	 * This specifies the add op that adds a value to a total to yield a new total.
+	 * @param value the value of the integer to be added to the total
+	 */
+	public void add (int value) {
+		
+	}
+	
+	/**
+	 * This specifies the subtract op that subtracts a value from the total to yield a new total. 
+	 * @param value the value of the integer to be subtracted from the total 
+	 */
+	public void subtract (int value) {
+		
+	}
+	
+	/**
+	 * returns the String that keeps record of all the 
+	 * operations completed 
+	 */
+	public String toString () {
+		return "";
+	}
+
+	/**
+	 * resets the total to 0 as well as all the operations done
+	 */
+	public void clear() {
+	
+	}
+}
